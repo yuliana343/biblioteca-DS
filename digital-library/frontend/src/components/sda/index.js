32bbb16a8dbe59f6e069e14cb22bdeeb3f0960cf
231165-design <231165@unamba.edu.pe>
2026-01-19 11:02:08 -0500

    Update index.js

--- digital-library/frontend/src/components/sda/index.js
+++ digital-library/frontend/src/components/sda/index.js
@@ -1,4 +1,4 @@
-// src/components/common/index.js
+
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Sidebar } from './Sidebar';
@@ -7,7 +7,7 @@ export { default as Pagination } from './Pagination';
 export { default as LoadingSpinner, BookSkeleton, TableSkeleton, CardSkeleton } from './LoadingSpinner';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/main.css'; // Importar CSS global
+import './styles/main.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,14 +16,13 @@ root.render(
     <App />
   </React.StrictMode>
 );
-// frontend/src/components/auth/index.js
+
 export { default as LoginForm } from './LoginForm';
 export { default as RegisterForm } from './RegisterForm';
-// frontend/src/components/loans/index.js
+
 export { default as ActiveLoans } from './ActiveLoans';
 export { default as LoanHistory } from './LoanHistory';
 export { default as RenewLoanModal } from './RenewLoanModal';
-// frontend/src/components/reservations/index.js
-// import { ReservationList, CreateReservation } from './components/reservations';
+
 export { default as ReservationList } from './ReservationList';
 export { default as CreateReservation } from './CreateReservation';
